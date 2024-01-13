aa6ab40923f769dc35a18437721b4da4ce02ada0
Carlos Martinez <carlos.martinez@devzeros.com>
2024-01-13 13:48:31 -0500

    Mi primer mensaje

---
+++ python.py
@@ -0,0 +1,12 @@
+a=int(input('coloque un numero'))
+b=int(input('coloque un segundo numero'))
+def sumatoria ():
+    c=a+b
+    print(c)
+    if (c%2==0):
+        print('Es par')
+    else:
+        print('Es imapar')
+    return c
+sumatoria()
+
